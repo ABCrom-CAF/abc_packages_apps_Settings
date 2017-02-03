4524bddca2a8f155a0f4a0fefeff0358b08ca759
ezio84 <brabus84@gmail.com>
2017-02-03 16:40:55 +0100

    Allow to quickly open Running Services from QS panel [2/2]
    @Mazda-- : Remove text from actionbar / Use overflow menu
    
    ezio: Running services: update actionbar title accordingly

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -193,4 +193,6 @@ public class Settings extends SettingsActivity {
     public static class SupportDashboardActivity extends SettingsActivity {}
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
 
+    // Abc Additions.
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
